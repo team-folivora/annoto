a629332f91ac0f4b349890cd269de30573cb11b7
Benedikt Weber <benedikt.web23@gmail.com>
2022-03-25 12:02:29 +0100

    Add pylint to workflow

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,5 @@
+'''Module documentation'''
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''Function documentation'''
     return 'Hello World!'
-
 
 if __name__ == "__main__":
     app.run()

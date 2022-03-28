88ef09d9ca0d92149740538005df613f82983587
Jannis Berndt <berndtjannis@gmail.com>
2022-03-28 17:17:55 +0200

    Add tests and formatting

--- api/tests/integration/test_integration.py
+++ api/tests/integration/test_integration.py
@@ -1,5 +1,32 @@
+from flask import Response
 import pytest
 
+from app import *
 
-def test_integration():
-    assert True
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+def test_image(client):
+    response:Response = client.get("/image")
+    assert response.content_type == "image/jpg"
+    assert response.status_code == 200

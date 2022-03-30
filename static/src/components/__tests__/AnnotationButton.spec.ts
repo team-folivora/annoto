a5b764215cb3906de4e22b696ac536a816f1f5ed
Jasper Sauer <jasper.sauer@student.hpi.de>
2022-03-30 13:47:44 +0200

    added probs for build job

--- static/src/components/__tests__/AnnotationButton.spec.ts
+++ static/src/components/__tests__/AnnotationButton.spec.ts
@@ -3,22 +3,22 @@ import AnnotationButton from "../AnnotationButton.vue";
 
 describe("Button", () => {
   it("exists", () => {
-    mount(AnnotationButton);
+    mount(AnnotationButton), {probs: { text: String }};
   });
 
   it("renders properly", () => {
-    mount(AnnotationButton);
+    mount(AnnotationButton), {probs: { text: String }};
     cy.get("button").should("be.visible");
   });
 
   it("is not disabled", () => {
-    mount(AnnotationButton);
+    mount(AnnotationButton), {probs: { text: String }};
     cy.get("button").should("not.be.disabled");
   });
 
   it("shows specific label", () => {
     const str = "Label: ";
-    mount(AnnotationButton, { props: {} });
+    mount(AnnotationButton), {probs: { text: String }};
     cy.get("button").contains(str);
   });
 });

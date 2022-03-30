49c1f00ecef0e4b9ace96c2853ae32a809c4774a
Benedikt Weber <benedikt.web23@gmail.com>
2022-03-30 17:55:08 +0200

    Enable auto reload

--- api/mod/__main__.py
+++ api/mod/__main__.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run("mod.src.app:APP", host="127.0.0.1", port=5000)
+    uvicorn.run("mod.src.app:APP", host="127.0.0.1", port=5000, reload=True)

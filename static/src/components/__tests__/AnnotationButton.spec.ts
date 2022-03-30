ff4de760f7cb63dd9c0a12606ff5341bcc194158
jdsauer <jdsauer@users.noreply.github.com>
2022-03-30 11:48:06 +0000

    Prettified Code!

--- static/src/components/__tests__/AnnotationButton.spec.ts
+++ static/src/components/__tests__/AnnotationButton.spec.ts
@@ -3,22 +3,22 @@ import AnnotationButton from "../AnnotationButton.vue";
 
 describe("Button", () => {
   it("exists", () => {
-    mount(AnnotationButton), {probs: { text: String }};
+    mount(AnnotationButton), { probs: { text: String } };
   });
 
   it("renders properly", () => {
-    mount(AnnotationButton), {probs: { text: String }};
+    mount(AnnotationButton), { probs: { text: String } };
     cy.get("button").should("be.visible");
   });
 
   it("is not disabled", () => {
-    mount(AnnotationButton), {probs: { text: String }};
+    mount(AnnotationButton), { probs: { text: String } };
     cy.get("button").should("not.be.disabled");
   });
 
   it("shows specific label", () => {
     const str = "Label: ";
-    mount(AnnotationButton), {probs: { text: String }};
+    mount(AnnotationButton), { probs: { text: String } };
     cy.get("button").contains(str);
   });
 });

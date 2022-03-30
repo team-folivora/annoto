9034c10afeb08bb19f6d47729e5ec4436ecf476d
Benedikt Weber <benedikt.web23@gmail.com>
2022-03-30 16:58:00 +0200

    Expect hash value for comparison

--- api/mod/src/app.py
+++ api/mod/src/app.py
@@ -26,25 +26,37 @@ async def get_image() -> FileResponse:
 
 
 class Annotation(BaseModel):
-    """Stores an annotation"""
+    """An annotation as it is provided by the API"""
 
     label: str
+    hash: str
+
+
+class StorableAnnotation(Annotation):
+    """An annotation as it is stored on disk"""
+
+    src: str
 
 
 @APP.post("/images/{src}", status_code=204)
 async def save_annotation(src: str, annotation: Annotation) -> None:
     """Saves the annotation for the specified image"""
-    annotation = {
-        "src": src,
-        "label": annotation.label,
-    }
-
     image_file = SETTINGS.data_folder.joinpath(src)
     if not image_file.is_file():
         raise HTTPException(status_code=404, detail="File not found")
+
     with open(image_file, "rb") as file:
-        annotation["hash"] = hashlib.sha256(file.read()).hexdigest()
+        local_hash = hashlib.sha256(file.read()).hexdigest()
+        if local_hash != annotation.hash:
+            raise HTTPException(
+                status_code=400,
+                detail="Hash values of the provided annotation and the local source do not match!",
+            )
+
+    annotation = StorableAnnotation(
+        label=annotation.label, hash=annotation.hash, src=src
+    )
 
     annotation_file = SETTINGS.data_folder.joinpath(f"{src}.annnotation.json")
     with open(annotation_file, "w", encoding="utf-8") as file:
-        file.write(json.dumps(annotation, indent=4))
+        file.write(json.dumps(annotation.__dict__, indent=4))

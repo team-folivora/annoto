3b42df417b8c29884e2b97612dfc4b0ef9042e6a
Jasper Sauer <jasper.sauer@student.hpi.de>
2022-03-30 13:06:43 +0200

    tests for button

---
+++ static/src/components/__tests__/Button.spec.ts
@@ -0,0 +1,26 @@
+import { mount } from "@cypress/vue"
+import Button from "../Button.vue"
+
+describe("Button", () => {
+   
+    it("exists", () => {
+      mount(Button);
+    });
+
+    it("renders properly", () => {
+        mount(Button);
+        cy.get('button').should('be.visible')
+    });
+
+    it("is not disabled", () => {
+        mount(Button);
+        cy.get('button').should('not.be.disabled')
+    });
+
+    it("shows specific label", () => {
+        const str = "Label: "   
+        mount(Button, { props: {} });
+        cy.get('button').contains(str)
+    });
+
+});

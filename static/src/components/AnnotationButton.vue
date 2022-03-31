865ed0911895c9f29573a8e295e4d9e3be7bc719
Jannis Berndt <berndtjannis@gmail.com>
2022-03-31 11:48:01 +0200

    Add docstrings

--- static/src/components/AnnotationButton.vue
+++ static/src/components/AnnotationButton.vue
@@ -1,12 +1,22 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+/**
+ * A Button for annotating a datafile
+ */
 export default defineComponent({
   props: {
+    /**
+     * The label of the button.
+     * Is displayed on the button and passed to the backend to save the annotation.
+     */
     text: { type: String, required: true }
   },
 
   methods: {
+    /**
+     * Gets executed when the user clicks on the button
+     * @param text printed to the console
+     */
     log(text: String): void {
       console.log("Hallo Mami" + "-" + text);
     }
--- static/src/components/ImageDisplay.vue
+++ static/src/components/ImageDisplay.vue
@@ -1,8 +1,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+/**
+ * Displays an image specified by `src`
+ */
 export default defineComponent({
   props: {
+    /**
+     * Source from where the image should be loaded.
+     */
     src: { type: String, required: true }
   },
 })

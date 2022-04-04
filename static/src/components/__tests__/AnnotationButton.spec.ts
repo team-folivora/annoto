74808d1b3f74469ee7957f62e53c860470a4e3c8
Jannis Berndt <berndtjannis@gmail.com>
2022-04-04 09:58:11 +0200

    Fix tests

--- static/src/components/__tests__/AnnotationButton.spec.ts
+++ static/src/components/__tests__/AnnotationButton.spec.ts
@@ -3,22 +3,22 @@ import AnnotationButton from "../AnnotationButton.vue";
 
 describe("Button", () => {
   it("exists", () => {
-    mount(AnnotationButton, { props: { label: "Test" } });
+    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg" } });
   });
 
   it("renders properly", () => {
-    mount(AnnotationButton, { props: { label: "Test" } });
+    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg" } });
     cy.get("button").should("be.visible");
   });
 
   it("is not disabled", () => {
-    mount(AnnotationButton, { props: { label: "Test" } });
+    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg"} });
     cy.get("button").should("not.be.disabled");
   });
 
   it("shows specific label", () => {
     const label = "Cypress";
-    mount(AnnotationButton, { props: { label: label } });
+    mount(AnnotationButton, { props: { label: label, src: "/images/test.jpg"} });
     cy.get("button").contains(label);
   });
 });

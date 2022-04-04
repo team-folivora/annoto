1de3bce14c2a2c0331d80c3f28eb97864476439d
jb3rndt <jb3rndt@users.noreply.github.com>
2022-04-04 07:59:02 +0000

    Prettified Code!

--- static/src/components/__tests__/AnnotationButton.spec.ts
+++ static/src/components/__tests__/AnnotationButton.spec.ts
@@ -3,22 +3,30 @@ import AnnotationButton from "../AnnotationButton.vue";
 
 describe("Button", () => {
   it("exists", () => {
-    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg" } });
+    mount(AnnotationButton, {
+      props: { label: "Test", src: "/images/test.jpg" },
+    });
   });
 
   it("renders properly", () => {
-    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg" } });
+    mount(AnnotationButton, {
+      props: { label: "Test", src: "/images/test.jpg" },
+    });
     cy.get("button").should("be.visible");
   });
 
   it("is not disabled", () => {
-    mount(AnnotationButton, { props: { label: "Test", src: "/images/test.jpg"} });
+    mount(AnnotationButton, {
+      props: { label: "Test", src: "/images/test.jpg" },
+    });
     cy.get("button").should("not.be.disabled");
   });
 
   it("shows specific label", () => {
     const label = "Cypress";
-    mount(AnnotationButton, { props: { label: label, src: "/images/test.jpg"} });
+    mount(AnnotationButton, {
+      props: { label: label, src: "/images/test.jpg" },
+    });
     cy.get("button").contains(label);
   });
 });

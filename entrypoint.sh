0132b1848fa2618899f3c80c94ba1f960cdc2280
Benedikt Weber <benedikt.web23@gmail.com>
2022-04-05 14:59:18 +0200

    Run npm i without updating package-lock

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ tmux send-keys -t main-session.0 "source venv/bin/activate" ENTER
 tmux send-keys -t main-session.0 "pip install -r requirements-dev.txt" ENTER
 tmux send-keys -t main-session.0 "python3 -m mod &> >(tee ~/output-api.log)" ENTER
 tmux send-keys -t main-session.1 "cd ~/annoto/static" ENTER
-tmux send-keys -t main-session.1 "npm install" ENTER
+tmux send-keys -t main-session.1 "npm install --no-save" ENTER
 tmux send-keys -t main-session.1 "npm run dev &> >(tee ~/output-static.log)" ENTER
 tmux a -t main-session
 

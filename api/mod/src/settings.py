91d9357003f772ba065a7817c47fc78c6927e13b
Jannis Berndt <berndtjannis@gmail.com>
2022-04-05 11:30:02 +0200

    Add docstring

--- api/mod/src/settings.py
+++ api/mod/src/settings.py
@@ -11,11 +11,18 @@ PYTHON_ENV = os.getenv("PYTHON_ENV", "development")
 
 
 class Settings(BaseSettings):
-    """Basic settings class"""
+    """
+    Basic settings class. Loads class attributes with the following priority:
+    1. env variables
+    2. values loaded from dotenv file according to PYTHON_ENV
+    3. defaults specified here
+    """
 
     data_folder: Path = Path.home().joinpath(".annoto")
 
     class Config:
+        """Reads the dotenv file"""
+
         env_file = f".env.{PYTHON_ENV}"
 
 

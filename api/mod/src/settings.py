dc2181d8da89de14bb78d3b885de79160c8b2aaa
Jannis Berndt <berndtjannis@gmail.com>
2022-04-05 10:44:08 +0200

    Read dotenv by pydantic

--- api/mod/src/settings.py
+++ api/mod/src/settings.py
@@ -2,15 +2,21 @@
 This module contains the settings
 """
 
+import os
 from pathlib import Path
 
 from pydantic import BaseSettings
+
+PYTHON_ENV = os.getenv("PYTHON_ENV", "development")
 
 
 class Settings(BaseSettings):
     """Basic settings class"""
 
     data_folder: Path = Path.home().joinpath(".annoto")
+
+    class Config:
+        env_file = f".env.{PYTHON_ENV}"
 
 
 SETTINGS = Settings()

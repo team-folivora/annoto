58f62ae994079e32ce20fb10c088fdfa0b484e2e
Benedikt Weber <benedikt.web23@gmail.com>
2022-04-05 20:38:55 +0200

    Introduce api generator

---
+++ api/mod/__dumpapi__.py
@@ -0,0 +1,9 @@
+from fastapi.openapi.utils import get_openapi
+from mod.src.app import APP
+
+openapi_schema = get_openapi(
+    title="Annoto API",
+    version="v1",
+    routes=APP.routes,
+)
+print(openapi_schema)

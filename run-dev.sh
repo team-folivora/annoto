dae9f58327eb604ca06e9e89ca9d51d6bcfe46fb
Benedikt Weber <benedikt.web23@gmail.com>
2022-04-05 15:10:18 +0200

    Catch SIGINT

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
+trap 'docker container kill annoto' INT
+
 docker build --tag annoto .  
 docker run --rm -it  --mount type=bind,source=$SCRIPT_DIR,target=/home/a/annoto --network host --name annoto annoto
-
-docker container kill annoto

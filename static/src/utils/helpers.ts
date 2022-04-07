e487d525bffc50efd92f065c29748dc4b2c34c6b
Benedikt Weber <benedikt.web23@gmail.com>
2022-04-07 10:01:06 +0200

    Fix linting

--- static/src/utils/helpers.ts
+++ static/src/utils/helpers.ts
@@ -1,4 +1,4 @@
 export function getUrl(path: string): string {
-  let apiUrl = import.meta.env.VITE_API_URL?.toString() || "";
+  const apiUrl = import.meta.env.VITE_API_URL?.toString() || "";
   return `${apiUrl}/${path}?`;
 }

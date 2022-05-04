63f61052f5d51a83109f63b6ab3944a0b406b317
Benedikt Weber <benedikt.web23@gmail.com>
2022-05-04 16:14:47 +0200

    Regenerate REST Client

---
+++ static/src/api/models/LoginResponse.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Login token (JWT)
+ */
+export type LoginResponse = {
+    /**
+     * The generated JWT access token
+     */
+    access_token: string;
+};

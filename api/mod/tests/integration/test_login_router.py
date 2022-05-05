031578d14aeba79569b33e083bd1a188b87b6d74
Benedikt Weber <benedikt.web23@gmail.com>
2022-05-05 10:31:06 +0200

    Add additional tests

--- api/mod/tests/integration/test_login_router.py
+++ api/mod/tests/integration/test_login_router.py
@@ -3,6 +3,7 @@ Module for integration tests
 """
 
 from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
 
 
 def test_login(client: TestClient) -> None:
@@ -30,3 +31,35 @@ def test_invalid_password_returns_401(client: TestClient) -> None:
         json={"email": "team@folivora.online", "password": "wrong_password"},
     )
     assert response.status_code == 401
+
+
+def test_login_token_is_usable(client: TestClient) -> None:
+    """Test POST /login"""
+    response = client.post(
+        "/login/",
+        json={"email": "team@folivora.online", "password": "password"},
+    )
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    response = client.get("/tasks", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+
+def test_login_token_expires(client: TestClient, mocker: MockerFixture) -> None:
+    """Test POST /login"""
+    mocker.patch("time.time", lambda: 0)
+    response = client.post(
+        "/login/",
+        json={"email": "team@folivora.online", "password": "password"},
+    )
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    mocker.patch("time.time", lambda: 1e9)
+    response = client.get("/tasks", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 403
+
+
+def test_login_is_required(client: TestClient) -> None:
+    """Test POST /tasks"""
+    response = client.get("/tasks")
+    assert response.status_code == 403

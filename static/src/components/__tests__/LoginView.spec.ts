00b785e891cb4e5074d9a4fc8c53d68a0fc3eb67
Jannis Berndt <berndtjannis@gmail.com>
2022-05-10 11:12:36 +0200

    LoginView tests

--- static/src/components/__tests__/LoginView.spec.ts
+++ static/src/components/__tests__/LoginView.spec.ts
@@ -1,5 +1,19 @@
 import { mount } from "@cypress/vue";
 import LoginView from "../LoginView.vue";
+import { ElMessage } from "element-plus";
+import {
+  intercept_login,
+  intercept_login_with_failure,
+} from "../../../cypress/integration/utils";
+import { OpenAPI } from "@/api/core/OpenAPI";
+
+beforeEach(() => {
+  OpenAPI.BASE = "http://localhost:5000";
+});
+
+afterEach(() => {
+  ElMessage.closeAll();
+});
 
 describe("LoginView", () => {
   it("exists", () => {
@@ -8,16 +22,61 @@ describe("LoginView", () => {
 
   it("has a field for email", () => {
     mount(LoginView);
-    cy.get("i-input[name='email']");
+    cy.get("input[name='email']");
   });
 
   it("has a field for password", () => {
     mount(LoginView);
-    cy.get("i-input[name='password']");
+    cy.get("input[name='password']");
   });
 
   it("has a submit button", () => {
     mount(LoginView);
-    cy.get("i-button#submit");
+    cy.get("button#submit");
+  });
+
+  it("warns if no password set", () => {
+    mount(LoginView);
+    cy.get("input[name='email']")
+      .type("testmail")
+      .get("button#submit")
+      .click()
+      .get("html")
+      .contains("E-Mail and Password need to be set...");
+  });
+
+  it("warns if no email set", () => {
+    mount(LoginView);
+    cy.get("input[name='password']")
+      .type("password")
+      .get("button#submit")
+      .click()
+      .get("html")
+      .contains("E-Mail and Password need to be set...");
+  });
+
+  it("emits login with correct credentials", () => {
+    // spying on the emitted events not working yet
+    intercept_login();
+    mount(LoginView);
+    cy.get("input[name='email']")
+      .type("team@folivora.online")
+      .get("input[name='password']")
+      .type("password")
+      .get("button#submit")
+      .click();
+  });
+
+  it("shows failure message with wrong credentials", () => {
+    intercept_login_with_failure();
+    mount(LoginView);
+    cy.get("input[name='email']")
+      .type("team@folivora.online")
+      .get("input[name='password']")
+      .type("password")
+      .get("button#submit")
+      .click()
+      .get("html")
+      .contains("E-Mail or Password is not correct...");
   });
 });

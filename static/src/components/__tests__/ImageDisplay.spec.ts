317e3cbe696a733a55d67a32de3df44435e023dd
Jannis Berndt <berndtjannis@gmail.com>
2022-05-10 10:37:14 +0200

    ImageDisplay tests

--- static/src/components/__tests__/ImageDisplay.spec.ts
+++ static/src/components/__tests__/ImageDisplay.spec.ts
@@ -1,5 +1,11 @@
 import { mount } from "@cypress/vue";
 import ImageDisplay from "@/components/ImageDisplay.vue";
+import { OpenAPI } from "@/api/core/OpenAPI";
+import { intercept_get_image, intercept_get_image_with_failure } from "../../../cypress/integration/utils";
+
+beforeEach(() => {
+  OpenAPI.BASE = "http://localhost:5000";
+});
 
 describe("ImageDisplay", () => {
   it("exists", () => {
@@ -9,5 +15,31 @@ describe("ImageDisplay", () => {
         imageId: "sloth.jpg",
       },
     });
+  });
+
+  it("shows image", () => {
+    intercept_get_image();
+    mount(ImageDisplay, {
+      props: {
+        taskId: "ecg-qrs-classification-physiodb",
+        imageId: "sloth.jpg",
+      },
+    }).get('img')
+    .should('be.visible')
+    .and(($img) => {
+      // "naturalWidth" and "naturalHeight" are set when the image loads
+      expect($img[0].naturalWidth).to.be.greaterThan(0)
+    });
+  });
+
+  it("shows nothing if image load fails", () => {
+    intercept_get_image_with_failure();
+    mount(ImageDisplay, {
+      props: {
+        taskId: "ecg-qrs-classification-physiodb",
+        imageId: "sloth.jpg",
+      },
+    }).get('img')
+    .should('not.be.visible');
   });
 });

5aeb665efc9f6e9a6434e5d60901f472cee8c458
Benedikt Weber <44091658+bewee@users.noreply.github.com>
2022-05-12 13:13:13 +0200

    Update api/mod/tests/conftest.py
    Co-authored-by: Jannis Berndt <berndtjannis@gmail.com>

--- api/mod/tests/conftest.py
+++ api/mod/tests/conftest.py
@@ -18,7 +18,7 @@ from mod.src.settings import SETTINGS
 
 
 def setup_test_db() -> None:
-    """Apply migrations to test database and load test data"""
+    """Apply migrations to test database and load test data. Is executed exactly once before the tests are run. Needs no cleanup because the database lies in a temporary folder."""
     config = Config(str(Path.cwd().joinpath("alembic.ini")))
     config.set_main_option(
         "script_location", str(Path.cwd().joinpath("mod").joinpath("alembic"))

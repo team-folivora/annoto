57af9201bbbccad0839207b6d1b1c1b1b90ea044
Jannis Berndt <berndtjannis@gmail.com>
2022-05-24 17:08:44 +0200

    Implement different Task types in backend
    Co-authored-by: n-koenig <n-koenig@users.noreply.github.com>

--- api/mod/src/models/task.py
+++ api/mod/src/models/task.py
@@ -1,25 +1,61 @@
 """Provides classes for Tasks"""
 
-from typing import List
+import json
+from pathlib import Path
+from typing import List, Union
 
 from pydantic import BaseModel, Field
+from enum import Enum
 
 
-class Task(BaseModel):
+class TaskType(Enum):
+    UNKNOWN = "unknown"
+    IMAGE_CLASSIFICATION = "image_classification"
+    FHIR_ECG_ANNOTATION = "fhir_ecg_annotation"
+
+
+class BaseTask(BaseModel):
     """
     A labeling task
     """
+
+    type_id: TaskType = Field(
+        ...,
+        description="The type of the task",
+        example=TaskType.FHIR_ECG_ANNOTATION,
+    )
 
     id: str = Field(
         ...,
         description="The identifier of this labelling task",
         example="ecg-qrs-classification-physiodb",
     )
+
     description: str = Field(
         ...,
         description="A brief description of this labelling task",
         example="Classifying the QRS complex of some ECGs",
     )
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "SpecificTask":
+        """
+        Creates a task from a type id
+        """
+        return task_types[TaskType(data["type_id"])](**data)
+
+    @classmethod
+    def load_from_file(cls, path: Path) -> "SpecificTask":
+        """
+        Loads a task from a file
+        """
+        if not path.exists():
+            raise Exception("Task file not found")
+        with open(path, "r", encoding="utf-8") as file:
+            return cls.from_dict(json.load(file))
+
+
+class ImageTask(BaseTask):
     labels: List[str] = Field(
         ...,
         description="List of labels",
@@ -31,3 +67,19 @@ class Task(BaseModel):
             "Other",
         ],
     )
+
+
+class FHIRECGTask(BaseTask):
+    pass
+
+
+task_types = {
+    TaskType.IMAGE_CLASSIFICATION: ImageTask,
+    TaskType.FHIR_ECG_ANNOTATION: FHIRECGTask,
+}
+
+SpecificTask = Union[ImageTask, FHIRECGTask]
+
+
+class TaskNotFoundException(Exception):
+    pass
--- api/mod/src/routers/tasks.py
+++ api/mod/src/routers/tasks.py
@@ -4,9 +4,11 @@ import json
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Path
+from mod.src.models.task import TaskNotFoundException
+from mod.src.models.task import SpecificTask
 
 from mod.src.auth.auth_bearer import JWTBearer
-from mod.src.models.task import Task
+from mod.src.models.task import BaseTask
 from mod.src.settings import SETTINGS
 
 ROUTER = APIRouter(
@@ -17,35 +19,26 @@ ROUTER = APIRouter(
 
 @ROUTER.get(
     "/",
-    response_model=List[Task],
+    response_model=List[BaseTask],
     operation_id="get_tasks",
     dependencies=[Depends(JWTBearer())],
 )
-async def get_tasks() -> List[Task]:
+async def get_tasks() -> List[BaseTask]:
     """Get a list of all available labeling tasks"""
     tasks_file = SETTINGS.data_folder.joinpath("tasks.json")
     tasks = []
     task_ids = []
-    if not tasks_file.exists():
-        raise HTTPException(status_code=500, detail="Tasks integrity violated")
     with open(tasks_file, "r", encoding="utf-8") as file:
         task_ids = json.load(file)
     for task_id in task_ids:
         task_file = SETTINGS.data_folder.joinpath(task_id).joinpath("task.json")
-        if not task_file.exists():
-            raise HTTPException(status_code=500, detail="Tasks integrity violated")
-        with open(
-            SETTINGS.data_folder.joinpath(task_id).joinpath("task.json"),
-            "r",
-            encoding="utf-8",
-        ) as file:
-            tasks.append(Task(**json.load(file)))
+        tasks.append(BaseTask.load_from_file(task_file))
     return tasks
 
 
 @ROUTER.get(
     "/{task_id}",
-    response_model=Task,
+    response_model=SpecificTask,
     responses={
         404: {"description": "Task not found!"},
     },
@@ -54,14 +47,13 @@ async def get_tasks() -> List[Task]:
 )
 async def get_task(
     task_id: str = Path(..., example="ecg-qrs-classification-physiodb"),
-) -> Task:
+) -> SpecificTask:
     """Get all information about a labelling task"""
-    task_folder = SETTINGS.data_folder.joinpath(task_id)
-    task_file = task_folder.joinpath("task.json")
-    if not (task_folder.exists() and task_file.exists()):
+    task_file = SETTINGS.data_folder.joinpath(task_id).joinpath("task.json")
+    try:
+        return BaseTask.load_from_file(task_file)
+    except TaskNotFoundException:
         raise HTTPException(
             status_code=404,
             detail="Task not found!",
         )
-    with open(task_file, "r", encoding="utf-8") as file:
-        return json.load(file)
